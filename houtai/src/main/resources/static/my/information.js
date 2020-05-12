e4aebe4f95423f2b92a691f9823e0dfc9ef7905d
zhangyanshen <zhangyanshen@inner.sss.com>
2020-05-12 15:46:13 +0800

    no

---
+++ houtai/src/main/resources/static/my/information.js
@@ -0,0 +1,20 @@
+// alert("hhh")
+// //昵称
+// // var nicheng = $("#nicheng").val();
+// // //qq号
+// // var qqhao = $("#qqhao").val();
+// // //phone
+// // var phone = $("#phone").val();
+// // //丢失日期
+// // var frtime = $("#frtime").val();
+// // var totime = $("#totime").val();
+// // //备注
+// // var beizhu = $("#beizhu").val();
+//
+// function demo() {
+//     alert($(" #nicheng ").val())
+// }
+// window.onload = function () {
+//
+//     var username = sessionStorage.getItem("username");
+// }
